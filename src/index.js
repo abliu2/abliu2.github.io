3c1378b56b6bd23774d65a6aea3e39eab125f5c7
Austin Liu <aliu315@bloomberg.net>
2019-07-29 23:09:21 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const HEADER_CONTENT = `
+# To Infinity... and Beyond?
+A history of NASA and how the public interest in space exploration has shifted over time.
+`;
+
+const CHAPTER_TITLE_1 = `# Chapter 1: The Birth of NASA`;
+const CHAPTER_TITLE_2 = `# Chapter 2: Soviet Russia gets a Head Start`;
+const CHAPTER_TITLE_3 = `# Chapter 3: "We choose to go to the Moon"`;
+const CHAPTER_TITLE_4 = `# Chapter 4: The Apollo Missions and the Decade of Innovation`;
+const CHAPTER_TITLE_5 = `# Chapter 5: Declining Interest in Space`;
+const CHAPTER_TITLE_6 = `# Chapter 6: The Rise of the Commercial Space Economy`;
+
+// Chapter Content
+const CHAPTER_CONTENT_1 = `NASA ...`;
+
+const CHAPTER_CONTENT_2 = `Soviet Russia ...`;
+
+const CHAPTER_CONTENT_3 = `JFK ...`;
+
+const CHAPTER_CONTENT_4 = `Apollo ...`;
+
+const CHAPTER_CONTENT_5 = `Declining Interest in Space ...`;
+
+const CHAPTER_CONTENT_6 = `Commercial Space Economy ...`;
