5ca60a41ecebffa22313da9b506f01ff53e73757
Austin Liu <aliu315@bloomberg.net>
2019-07-30 23:01:19 -0400

    Progress

--- src/index.js
+++ src/index.js
@@ -22,3 +22,26 @@ const CHAPTER_CONTENT_4 = `Apollo ...`;
 const CHAPTER_CONTENT_5 = `Declining Interest in Space ...`;
 
 const CHAPTER_CONTENT_6 = `Commercial Space Economy ...`;
+
+// Chart Constants & Stuff
+const CHART_MARGINS = {
+    top: 20,
+    right: 20,
+    bottom: 30,
+    left: 50,
+};
+
+const CHART_SECTION_D3 = d3.select(".chart_section");
+const CHART_HEIGHT = CHART_SECTION_D3.node().getBoundingClientRect().height - CHART_MARGINS.top - CHART_MARGINS.bottom;
+const CHART_WIDTH  = CHART_SECTION_D3.node().getBoundingClientRect().width - CHART_MARGINS.left - CHART_MARGINS.right;
+
+const CHART = d3.select(".chart")
+    .attr("width", CHART_WIDTH)
+    .attr("height", CHART_HEIGHT);
+
+let X_fxn = d3.scaleLinear().domain([1959, 2016]).range([0, CHART_WIDTH]);
+let Y_fxn = d3.scaleLinear().domain([0, 0.8]).range([CHART_HEIGHT, 0]);
+
+function setup_chapter_1_chart(data) {
+    X_fxn(3);
+}
